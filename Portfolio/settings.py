82a81dd631652ecfd9d638850c74e41e8ad1da6a
ahmedhuyu <ahmed.huyu@gmail.com>
2025-06-07 18:23:48 +0300

    fix mobile view

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-go+475a%2559tu_%4k^_drhb-@fxh+nujp!1916bsgx+95jz)-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['ahmedyunis.pythonanywhere.com']
 
